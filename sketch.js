951c71bbbb967504558c2bcf111d38273f6bee93
nai04092008 <145473632+nai04092008@users.noreply.github.com>
2023-10-19 09:49:39 -0300

    atualizando

--- sketch.js
+++ sketch.js
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.7.0/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.7.0/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
+function setup() {
+  createCanvas(700, 700);
+  background ("white")
+}
 
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-  </body>
-</html>
+function draw() {
+  stroke("white")
+  fill("black")
+
+ if(mouseIsPressed) {
+      rect(mouseX, mouseY, 50, 50)
+  } 
+}
+  
